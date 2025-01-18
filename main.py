c5d1d30b4e9834b16fcf402918825d9a1f6e1524
Bhavya Patel <bhavyapatel@bhavyas-mbp.lan>
2025-01-18 13:05:14 -0500

    Intilization of game window

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import sys
+
+pygame.init()
+
+screen = pygame.display.set_mode((400, 500))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.update()
